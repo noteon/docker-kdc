252f83fb9dcdd08f89669679c4024084a9a24a7d
qinghai <qinghai@gmail.com>
2017-06-28 13:58:38 +0800

    fix sh bug

--- start-mongod.sh
+++ start-mongod.sh
@@ -2,6 +2,6 @@ env KRB5_KTNAME=/home/ubuntu/git-projects/qh-docker-kdc/krb5.keytab \
 /user/bin/mongod \
 --port 27040 \
 --auth \
---smallfiles
+--smallfiles \
 --setParameter authenticationMechanisms=GSSAPI \
  --dbpath=/mnt/mongodb/mongodbEntKrb
